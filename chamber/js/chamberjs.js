c2a459f30f6ce6c8dcd2ea1bc2e27027bc5aaeca
siempreaprendiendo <jim20003@byui.edu>
2022-05-14 20:52:43 -0700

    lesson 4

---
+++ chamber/js/chamberjs.js
@@ -0,0 +1,31 @@
+let modDate = new Date(document.lastModified)
+let curDate = new Date()
+let year = curDate.getFullYear()
+let modYear = modDate.getFullYear()
+let modMonth = modDate.getMonth()
+let modDay = modDate.getDate()
+let modWkDay = modDate.getDay()
+let modHrs = modDate.getHours()
+let modMin = modDate.getMinutes()
+let modSec = modDate.getSeconds()
+
+
+document.getElementById("dateMod").innerHTML = `Last Modified: ${modMonth}/${modDay}/${modYear} ${modHrs}:${modMin}:${modSec}`;
+document.getElementById("year").innerText = year;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById('hamburgerBtn').classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn') 
+x.onclick = toggleMenu;
+
+const datefield = document.querySelector(".date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
