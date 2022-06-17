125c13af96299f44326186dde5f4151b32432a46
siempreaprendiendo <jim20003@byui.edu>
2022-06-17 12:36:30 -0700

    finish json

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -15,20 +15,24 @@ async function getProphets() {
     }
 }
 
-function buildCards(info) {
-    info.prophets.forEach(prophet => {
-        let card = document.createElement('sections');
+function buildCards(data) {
+    data.prophets.forEach(prophet => {
+        let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        let p = document.createElement('hp');
+        let p = document.createElement('p');
         let img = document.createElement('img');
+        let birthDate = document.createElement('p')
 
         h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
         p.innerHTML = `Location of Birth: <strong>${prophet.birthplace}</storng>`;
         img.setAttribute('src', `${prophet.imageurl}`);
+        img.setAttribute('alt', `Portrait of President ${prophet.name} ${prophet.lastname}`)
         img.setAttribute('loading', 'lazy');
+        birthDate.innerHTML = `${prophet.birthdate}`;
 
         card.append(h2);
         card.append(p);
+        card.append(birthDate)
         card.append(img);
         cards.append(card);
         
