6dc695c97dab6bf6ccf8cd9c51c357d89cfdeaca
siempreaprendiendo <jim20003@byui.edu>
2022-06-25 23:04:46 -0700

    chamber page weather

---
+++ chamber/js/weatherapi.js
@@ -0,0 +1,37 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Seattle&units=Imperial&appid=7671f53ea22020bf2f3a9e4bc7ae05f4`;
+
+apiFetch(url);
+
+async function apiFetch(apiURL) {
+
+    try {
+        const response = await fetch(apiURL);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data); // temp //
+            displayResults(data)
+
+        } else {
+            throw Error(await response.text());
+        } 
+    } catch (error) {
+        console.log(error);
+    }
+
+} 
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong> ${weatherData.main.temp.toFixed(1)}</strong>`;
+
+    const imagesrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src', imagesrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.innerHTML = desc;
+
+}
+
